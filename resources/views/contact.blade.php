766cac2bcb79cf4b0dd305b58261a9c36f218279
Cory Martin <murribu@gmail.com>
2015-04-30 14:01:51 -0500

    Correct mis-capitalization in Mugshot.jpg

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <section id="primary">
-      <img src="img/mugshot.jpg" alt="Photograph of Cory Martin" class="profile-photo">
+      <img src="img/Mugshot.jpg" alt="Photograph of Cory Martin" class="profile-photo">
       <h3>General Information</h3>
       <p>I've got a daily gig right now, but if you'd like to enquire about side-projects, please shoot me a tweet.</p>
       <p>Twitter is the best way to reach me. Hit me up and we'll chat!</p>
